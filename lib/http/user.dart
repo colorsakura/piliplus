guozhigq/pilipala

feat: 稍后再看&收藏夹播放全部

lib/http/user.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:html/parser.dart';
+import 'package:pilipala/models/video/later.dart';
 import '../common/constants.dart';
 import '../models/model_hot_video_item.dart';
 import '../models/user/fav_detail.dart';
@@ -427,5 +432,108 @@ class UserHttp {
     } else {
       return {'status': false, 'msg': res.data['message']};
     }
+  }
+
+  // 稍后再看播放全部
+  // static Future toViewPlayAll({required int oid, required String bvid}) async {
+  //   var res = await Request().get(
+  //     Api.watchLaterHtml,
+  //     data: {
+  //       'oid': oid,
+  //       'bvid': bvid,
+  //     },
+  //   );
+  //   String scriptContent =
+  //       extractScriptContents(parse(res.data).body!.outerHtml)[0];
+  //   int startIndex = scriptContent.indexOf('{');
+  //   int endIndex = scriptContent.lastIndexOf('};');
+  //   String jsonContent = scriptContent.substring(startIndex, endIndex + 1);
+  //   // 解析JSON字符串为Map
+  //   Map<String, dynamic> jsonData = json.decode(jsonContent);
+  //   // 输出解析后的数据
+  //   return {
+  //     'status': true,
+  //     'data': jsonData['resourceList']
+  //         .map((e) => MediaVideoItemModel.fromJson(e))
+  //         .toList()
+  //   };
+  // }
+
+  static List<String> extractScriptContents(String htmlContent) {
+    RegExp scriptRegExp = RegExp(r'<script>([\s\S]*?)<\/script>');
+    Iterable<Match> matches = scriptRegExp.allMatches(htmlContent);
+    List<String> scriptContents = [];
+    for (Match match in matches) {
+      String scriptContent = match.group(1)!;
+      scriptContents.add(scriptContent);
+    }
+    return scriptContents;
+  }
+
+  // 稍后再看列表
+  static Future getMediaList({
+    required int type,
+    required int bizId,
+    required int ps,
+    int? oid,
+  }) async {
+    var res = await Request().get(
+      Api.mediaList,
+      data: {
+        'mobi_app': 'web',
+        'type': type,
+        'biz_id': bizId,
+        'oid': oid ?? '',
+        'otype': 2,
+        'ps': ps,
+        'direction': false,
+        'desc': true,
+        'sort_field': 1,
+        'tid': 0,
+        'with_current': false,
+      },
+    );
+    if (res.data['code'] == 0) {
+      return {
+        'status': true,
+        'data': res.data['data']['media_list'] != null
+            ? res.data['data']['media_list']
+                .map<MediaVideoItemModel>(
+                    (e) => MediaVideoItemModel.fromJson(e))
+                .toList()
+            : []
+      };
+    } else {
+      return {'status': false, 'msg': res.data['message']};
+    }
+  }
+
+  // 解析收藏夹视频
+  static Future parseFavVideo({
+    required int mediaId,
+    required int oid,
+    required String bvid,
+  }) async {
+    var res = await Request().get(
+      'https://www.bilibili.com/list/ml$mediaId',
+      data: {
+        'oid': mediaId,
+        'bvid': bvid,
+      },
+    );
+    String scriptContent =
+        extractScriptContents(parse(res.data).body!.outerHtml)[0];
+    int startIndex = scriptContent.indexOf('{');
+    int endIndex = scriptContent.lastIndexOf('};');
+    String jsonContent = scriptContent.substring(startIndex, endIndex + 1);
+    // 解析JSON字符串为Map
+    Map<String, dynamic> jsonData = json.decode(jsonContent);
+    log('jsonData;${jsonData["resourceList"].first}');
+    return {
+      'status': true,
+      'data': jsonData['resourceList']
+          .map<MediaVideoItemModel>((e) => MediaVideoItemModel.fromJson(e))
+          .toList()
+    };
   }
 }

lib/pages/fav_detail/controller.dart
@@ -6,17 +6,17 @@ import 'package:pilipala/http/video.dart';
 import 'package:pilipala/models/user/fav_detail.dart';
 import 'package:pilipala/models/user/fav_folder.dart';
 import 'package:pilipala/pages/fav/index.dart';
+import 'package:pilipala/utils/utils.dart';
 
 class FavDetailController extends GetxController {
   FavFolderItemData? item;
-  Rx<FavDetailData> favDetailData = FavDetailData().obs;
 
   int? mediaId;
   late String heroTag;
   int currentPage = 1;
   bool isLoadingMore = false;
   RxMap favInfo = {}.obs;
-  RxList favList = [].obs;
+  RxList<FavDetailItemData> favList = <FavDetailItemData>[].obs;
   RxString loadingText = '加载中...'.obs;
   RxInt mediaCount = 0.obs;
   late String isOwner;
@@ -112,6 +112,24 @@ class FavDetailController extends GetxController {
             )
           ],
         );
+      },
+    );
+  }
+
+  Future toViewPlayAll() async {
+    final FavDetailItemData firstItem = favList.first;
+    final String heroTag = Utils.makeHeroTag(firstItem.bvid);
+    Get.toNamed(
+      '/video?bvid=${firstItem.bvid}&cid=${firstItem.cid}',
+      arguments: {
+        'videoItem': firstItem,
+        'heroTag': heroTag,
+        'sourceType': 'fav',
+        'mediaId': favInfo['id'],
+        'oid': firstItem.id,
+        'favTitle': favInfo['title'],
+        'favInfo': favInfo,
+        'count': favInfo['media_count'],
       },
     );
   }

lib/pages/later/controller.dart
@@ -6,6 +6,7 @@ import 'package:pilipala/http/user.dart';
 import 'package:pilipala/models/model_hot_video_item.dart';
 import 'package:pilipala/models/user/info.dart';
 import 'package:pilipala/utils/storage.dart';
+import 'package:pilipala/utils/utils.dart';
 
 class LaterController extends GetxController {
   final ScrollController scrollController = ScrollController();
@@ -48,7 +49,7 @@ class LaterController extends GetxController {
               aid != null ? '即将移除该视频，确定是否移除' : '即将删除所有已观看视频，此操作不可恢复。确定是否删除？'),
           actions: [
             TextButton(
-              onPressed: () => SmartDialog.dismiss(),
+              onPressed: SmartDialog.dismiss,
               child: Text(
                 '取消',
                 style: TextStyle(color: Theme.of(context).colorScheme.outline),
@@ -87,7 +88,7 @@ class LaterController extends GetxController {
           content: const Text('确定要清空你的稍后再看列表吗？'),
           actions: [
             TextButton(
-              onPressed: () => SmartDialog.dismiss(),
+              onPressed: SmartDialog.dismiss,
               child: Text(
                 '取消',
                 style: TextStyle(color: Theme.of(context).colorScheme.outline),
@@ -106,6 +107,21 @@ class LaterController extends GetxController {
             )
           ],
         );
+      },
+    );
+  }
+
+  // 稍后再看播放全部
+  Future toViewPlayAll() async {
+    final HotVideoItemModel firstItem = laterList.first;
+    final String heroTag = Utils.makeHeroTag(firstItem.bvid);
+    Get.toNamed(
+      '/video?bvid=${firstItem.bvid}&cid=${firstItem.cid}',
+      arguments: {
+        'videoItem': firstItem,
+        'heroTag': heroTag,
+        'sourceType': 'watchLater',
+        'count': laterList.length,
       },
     );
   }

lib/pages/video/detail/controller.dart
@@ -7,9 +7,11 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:ns_danmaku/ns_danmaku.dart';
 import 'package:pilipala/http/constants.dart';
+import 'package:pilipala/http/user.dart';
 import 'package:pilipala/http/video.dart';
 import 'package:pilipala/models/common/reply_type.dart';
 import 'package:pilipala/models/common/search_type.dart';
+import 'package:pilipala/models/video/later.dart';
 import 'package:pilipala/models/video/play/quality.dart';
 import 'package:pilipala/models/video/play/url.dart';
 import 'package:pilipala/models/video/reply/item.dart';
@@ -24,7 +26,10 @@ import '../../../models/video/subTitile/content.dart';
 import '../../../http/danmaku.dart';
 import '../../../plugin/pl_player/models/bottom_control_type.dart';
 import '../../../utils/id_utils.dart';
+import 'introduction/controller.dart';
+import 'reply/controller.dart';
 import 'widgets/header_control.dart';
+import 'widgets/watch_later_list.dart';
 
 class VideoDetailController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -37,9 +42,10 @@ class VideoDetailController extends GetxController
   Map videoItem = {};
   // 视频类型 默认投稿视频
   SearchType videoType = Get.arguments['videoType'] ?? SearchType.video;
+  // 页面来源 稍后再看 收藏夹
+  RxString sourceType = 'normal'.obs;
 
   /// tabs相关配置
-  int tabInitialIndex = 0;
   late TabController tabCtr;
   RxList<String> tabs = <String>['简介', '评论'].obs;
 
@@ -110,6 +116,9 @@ class VideoDetailController extends GetxController
   RxDouble sheetHeight = 0.0.obs;
   RxString archiveSourceType = 'dash'.obs;
   ScrollController? replyScrillController;
+  List<MediaVideoItemModel> mediaList = <MediaVideoItemModel>[];
+  RxBool isWatchLaterVisible = false.obs;
+  RxString watchLaterTitle = ''.obs;
 
   @override
   void onInit() {
@@ -119,9 +128,7 @@ class VideoDetailController extends GetxController
     if (argMap.containsKey('videoItem')) {
       var args = argMap['videoItem'];
       updateCover(args.pic);
-    }
-
-    if (argMap.containsKey('pic')) {
+    } else if (argMap.containsKey('pic')) {
       updateCover(argMap['pic']);
     }
 
@@ -160,6 +167,21 @@ class VideoDetailController extends GetxController
       bvid: bvid,
       videoType: videoType,
     );
+
+    sourceType.value = argMap['sourceType'] ?? 'normal';
+    isWatchLaterVisible.value =
+        sourceType.value == 'watchLater' || sourceType.value == 'fav';
+    if (sourceType.value == 'watchLater') {
+      watchLaterTitle.value = '稍后再看';
+      fetchMediaList();
+    }
+    if (sourceType.value == 'fav') {
+      watchLaterTitle.value = argMap['favTitle'];
+      queryFavVideoList();
+    }
+    tabCtr.addListener(() {
+      onTabChanged();
+    });
   }
 
   showReplyReplyPanel(oid, fRpid, firstFloor, currentReply, loadMore) {
@@ -560,5 +582,102 @@ class VideoDetailController extends GetxController
       replyScrillController?.animateTo(0,
           duration: const Duration(milliseconds: 300), curve: Curves.ease);
     }
+  }
+
+  void toggeleWatchLaterVisible(bool val) {
+    if (sourceType.value == 'watchLater' || sourceType.value == 'fav') {
+      isWatchLaterVisible.value = !isWatchLaterVisible.value;
+    }
+  }
+
+  // 获取稍后再看列表
+  Future fetchMediaList() async {
+    final Map argMap = Get.arguments;
+    var count = argMap['count'];
+    var res = await UserHttp.getMediaList(
+      type: 2,
+      bizId: userInfo.mid,
+      ps: count,
+    );
+    if (res['status']) {
+      mediaList = res['data'].reversed.toList();
+    } else {
+      SmartDialog.showToast(res['msg']);
+    }
+  }
+
+  // 稍后再看面板展开
+  showMediaListPanel() {
+    replyReplyBottomSheetCtr =
+        scaffoldKey.currentState?.showBottomSheet((BuildContext context) {
+      return MediaListPanel(
+        sheetHeight: sheetHeight.value,
+        mediaList: mediaList,
+        changeMediaList: changeMediaList,
+        panelTitle: watchLaterTitle.value,
+        bvid: bvid,
+        mediaId: Get.arguments['mediaId'],
+        hasMore: mediaList.length != Get.arguments['count'],
+      );
+    });
+    replyReplyBottomSheetCtr?.closed.then((value) {
+      isWatchLaterVisible.value = true;
+    });
+  }
+
+  // 切换稍后再看
+  Future changeMediaList(bvidVal, cidVal, aidVal, coverVal) async {
+    final VideoIntroController videoIntroCtr =
+        Get.find<VideoIntroController>(tag: heroTag);
+    bvid = bvidVal;
+    oid.value = aidVal ?? IdUtils.bv2av(bvid);
+    cid.value = cidVal;
+    danmakuCid.value = cidVal;
+    cover.value = coverVal;
+    queryVideoUrl();
+    clearSubtitleContent();
+    await getSubtitle();
+    setSubtitleContent();
+    // 重新请求评论
+    try {
+      /// 未渲染回复组件时可能异常
+      final VideoReplyController videoReplyCtr =
+          Get.find<VideoReplyController>(tag: heroTag);
+      videoReplyCtr.aid = aidVal;
+      videoReplyCtr.queryReplyList(type: 'init');
+    } catch (_) {}
+    videoIntroCtr.lastPlayCid.value = cidVal;
+    videoIntroCtr.bvid = bvidVal;
+    replyReplyBottomSheetCtr!.close();
+    await videoIntroCtr.queryVideoIntro();
+  }
+
+  // 获取收藏夹视频列表
+  Future queryFavVideoList() async {
+    final Map argMap = Get.arguments;
+    var mediaId = argMap['mediaId'];
+    var oid = argMap['oid'];
+    var res = await UserHttp.parseFavVideo(
+      mediaId: mediaId,
+      oid: oid,
+      bvid: bvid,
+    );
+    if (res['status']) {
+      mediaList = res['data'];
+    }
+  }
+
+  // 监听tabBarView切换
+  void onTabChanged() {
+    isWatchLaterVisible.value = tabCtr.index == 0;
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    plPlayerController.dispose();
+    tabCtr.removeListener(() {
+      onTabChanged();
+    });
   }
 }

colorsakura/piliplus

Co-authored-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

lib/http/user.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:PiliPalaX/http/loading_state.dart';
+import 'package:PiliPalaX/models/video/later.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:html/parser.dart';
 import '../common/constants.dart';
 import '../models/model_hot_video_item.dart';
 import '../models/user/fav_detail.dart';
@@ -470,5 +474,106 @@ class UserHttp {
     } else {
       return {'status': false};
     }
+  }
+
+  // 稍后再看播放全部
+  // static Future toViewPlayAll({required int oid, required String bvid}) async {
+  //   var res = await Request().get(
+  //     Api.watchLaterHtml,
+  //     data: {
+  //       'oid': oid,
+  //       'bvid': bvid,
+  //     },
+  //   );
+  //   String scriptContent =
+  //       extractScriptContents(parse(res.data).body!.outerHtml)[0];
+  //   int startIndex = scriptContent.indexOf('{');
+  //   int endIndex = scriptContent.lastIndexOf('};');
+  //   String jsonContent = scriptContent.substring(startIndex, endIndex + 1);
+  //   // 解析JSON字符串为Map
+  //   Map<String, dynamic> jsonData = json.decode(jsonContent);
+  //   // 输出解析后的数据
+  //   return {
+  //     'status': true,
+  //     'data': jsonData['resourceList']
+  //         .map((e) => MediaVideoItemModel.fromJson(e))
+  //         .toList()
+  //   };
+  // }
+  static List<String> extractScriptContents(String htmlContent) {
+    RegExp scriptRegExp = RegExp(r'<script>([\s\S]*?)<\/script>');
+    Iterable<Match> matches = scriptRegExp.allMatches(htmlContent);
+    List<String> scriptContents = [];
+    for (Match match in matches) {
+      String scriptContent = match.group(1)!;
+      scriptContents.add(scriptContent);
+    }
+    return scriptContents;
+  }
+
+  // 稍后再看列表
+  static Future getMediaList({
+    required int type,
+    required int bizId,
+    required int ps,
+    int? oid,
+  }) async {
+    var res = await Request().get(
+      Api.mediaList,
+      queryParameters: {
+        'mobi_app': 'web',
+        'type': type,
+        'biz_id': bizId,
+        'oid': oid ?? '',
+        'otype': 2,
+        'ps': ps,
+        'direction': false,
+        'desc': true,
+        'sort_field': 1,
+        'tid': 0,
+        'with_current': false,
+      },
+    );
+    if (res.data['code'] == 0) {
+      return {
+        'status': true,
+        'data': res.data['data']['media_list'] != null
+            ? res.data['data']['media_list']
+                .map<MediaVideoItemModel>(
+                    (e) => MediaVideoItemModel.fromJson(e))
+                .toList()
+            : []
+      };
+    } else {
+      return {'status': false, 'msg': res.data['message']};
+    }
+  }
+
+  // 解析收藏夹视频
+  static Future parseFavVideo({
+    required int mediaId,
+    required int oid,
+    required String bvid,
+  }) async {
+    var res = await Request().get(
+      'https://www.bilibili.com/list/ml$mediaId',
+      queryParameters: {
+        'oid': mediaId,
+        'bvid': bvid,
+      },
+    );
+    String scriptContent =
+        extractScriptContents(parse(res.data).body!.outerHtml)[0];
+    int startIndex = scriptContent.indexOf('{');
+    int endIndex = scriptContent.lastIndexOf('};');
+    String jsonContent = scriptContent.substring(startIndex, endIndex + 1);
+    // 解析JSON字符串为Map
+    Map<String, dynamic> jsonData = json.decode(jsonContent);
+    return {
+      'status': true,
+      'data': jsonData['resourceList']
+          .map<MediaVideoItemModel>((e) => MediaVideoItemModel.fromJson(e))
+          .toList()
+    };
   }
 }

lib/pages/fav_detail/controller.dart
@@ -4,6 +4,7 @@ import 'package:PiliPalaX/models/user/fav_detail.dart';
 import 'package:PiliPalaX/models/user/fav_folder.dart';
 import 'package:PiliPalaX/pages/common/multi_select_controller.dart';
 import 'package:PiliPalaX/utils/storage.dart';
+import 'package:PiliPalaX/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
@@ -122,5 +123,26 @@ class FavDetailController extends MultiSelectController {
         );
       },
     );
+  }
+
+  Future toViewPlayAll() async {
+    if (loadingState.value is Success) {
+      final FavDetailItemData firstItem =
+          (loadingState.value as Success).response.first;
+      final String heroTag = Utils.makeHeroTag(firstItem.bvid);
+      Get.toNamed(
+        '/video?bvid=${firstItem.bvid}&cid=${firstItem.cid}',
+        arguments: {
+          'videoItem': firstItem,
+          'heroTag': heroTag,
+          'sourceType': 'fav',
+          'mediaId': item.value.id,
+          'oid': firstItem.id,
+          'favTitle': item.value.title,
+          // 'favInfo': favInfo,
+          'count': item.value.mediaCount,
+        },
+      );
+    }
   }
 }

lib/pages/later/controller.dart
@@ -1,6 +1,7 @@
 import 'package:PiliPalaX/http/loading_state.dart';
 import 'package:PiliPalaX/models/model_hot_video_item.dart';
 import 'package:PiliPalaX/pages/common/multi_select_controller.dart';
+import 'package:PiliPalaX/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
@@ -161,5 +162,23 @@ class LaterController extends MultiSelectController {
     }
     SmartDialog.dismiss();
     SmartDialog.showToast(res['msg']);
+  }
+
+  // 稍后再看播放全部
+  Future toViewPlayAll() async {
+    if (loadingState.value is Success) {
+      final HotVideoItemModel firstItem =
+          (loadingState.value as Success).response.first;
+      final String heroTag = Utils.makeHeroTag(firstItem.bvid);
+      Get.toNamed(
+        '/video?bvid=${firstItem.bvid}&cid=${firstItem.cid}',
+        arguments: {
+          'videoItem': firstItem,
+          'heroTag': heroTag,
+          'sourceType': 'watchLater',
+          'count': (loadingState.value as Success).response.length,
+        },
+      );
+    }
   }
 }

lib/pages/video/detail/controller.dart
@@ -6,9 +6,15 @@ import 'package:PiliPalaX/common/widgets/icon_button.dart';
 import 'package:PiliPalaX/common/widgets/loading_widget.dart';
 import 'package:PiliPalaX/common/widgets/pair.dart';
 import 'package:PiliPalaX/common/widgets/segment_progress_bar.dart';
+import 'package:PiliPalaX/common/widgets/watch_later_list.dart';
 import 'package:PiliPalaX/http/danmaku.dart';
 import 'package:PiliPalaX/http/init.dart';
+import 'package:PiliPalaX/http/user.dart';
+import 'package:PiliPalaX/models/video/later.dart';
 import 'package:PiliPalaX/models/video/play/subtitle.dart';
+import 'package:PiliPalaX/pages/video/detail/introduction/controller.dart';
+import 'package:PiliPalaX/pages/video/detail/related/controller.dart';
+import 'package:PiliPalaX/pages/video/detail/reply/controller.dart';
 import 'package:PiliPalaX/utils/extension.dart';
 import 'package:canvas_danmaku/models/danmaku_content_item.dart';
 import 'package:dio/dio.dart';
@@ -226,6 +232,11 @@ class VideoDetailController extends GetxController
     imageStatus = false;
   }
 
+  // 页面来源 稍后再看 收藏夹
+  RxString sourceType = 'normal'.obs;
+  List<MediaVideoItemModel> mediaList = <MediaVideoItemModel>[];
+  RxString watchLaterTitle = ''.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -243,6 +254,17 @@ class VideoDetailController extends GetxController
         videoItem['pic'] = argMap['pic'];
       }
     }
+
+    sourceType.value = argMap['sourceType'] ?? 'normal';
+
+    if (sourceType.value == 'watchLater') {
+      watchLaterTitle.value = '稍后再看';
+      fetchMediaList();
+    } else if (sourceType.value == 'fav') {
+      watchLaterTitle.value = argMap['favTitle'];
+      queryFavVideoList();
+    }
+
     bool defaultShowComment =
         setting.get(SettingBoxKey.defaultShowComment, defaultValue: false);
     tabCtr = TabController(
@@ -269,6 +291,7 @@ class VideoDetailController extends GetxController
       floating: floating,
       heroTag: heroTag,
     );
+
     // CDN优化
     // enableCDN = setting.get(SettingBoxKey.enableCDN, defaultValue: true);
 
@@ -289,6 +312,78 @@ class VideoDetailController extends GetxController
       _blockLimit = GStorage.blockLimit;
       _blockSettings = GStorage.blockSettings;
       _blockColor = GStorage.blockColor;
+    }
+  }
+
+  // 获取稍后再看列表
+  Future fetchMediaList() async {
+    final Map argMap = Get.arguments;
+    var count = argMap['count'];
+    var res = await UserHttp.getMediaList(
+      type: 2,
+      bizId: userInfo.mid,
+      ps: count,
+    );
+    if (res['status']) {
+      mediaList = res['data'].reversed.toList();
+    } else {
+      SmartDialog.showToast(res['msg']);
+    }
+  }
+
+  // 稍后再看面板展开
+  showMediaListPanel() {
+    if (mediaList.isNotEmpty) {
+      childKey.currentState?.showBottomSheet(
+        (context) => MediaListPanel(
+          mediaList: mediaList,
+          changeMediaList: changeMediaList,
+          panelTitle: watchLaterTitle.value,
+          bvid: bvid,
+          mediaId: Get.arguments['mediaId'],
+          hasMore: mediaList.length != Get.arguments['count'],
+        ),
+      );
+    }
+  }
+
+  // 切换稍后再看
+  Future changeMediaList(bvid, cid, aid, cover) async {
+    try {
+      this.bvid = bvid;
+      oid.value = aid ?? IdUtils.bv2av(bvid);
+      this.cid.value = cid;
+      danmakuCid.value = cid;
+      videoItem['pic'] = cover;
+      queryVideoUrl();
+
+      Get.find<VideoReplyController>(tag: heroTag)
+        ..aid = aid
+        ..onRefresh();
+
+      Get.find<VideoIntroController>(tag: heroTag)
+        ..lastPlayCid.value = cid
+        ..bvid = bvid
+        ..queryVideoIntro();
+
+      Get.find<RelatedController>(tag: heroTag)
+        ..bvid = bvid
+        ..onRefresh();
+    } catch (_) {}
+  }
+
+  // 获取收藏夹视频列表
+  Future queryFavVideoList() async {
+    final Map argMap = Get.arguments;
+    var mediaId = argMap['mediaId'];
+    var oid = argMap['oid'];
+    var res = await UserHttp.parseFavVideo(
+      mediaId: mediaId,
+      oid: oid,
+      bvid: bvid,
+    );
+    if (res['status']) {
+      mediaList = res['data'];
     }
   }
 